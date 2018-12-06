ba47b4dc83b10ceb63957e15bb755a9f19d47964
Lindsay Karty <lmkarty@gmail.com>
2018-12-06 15:09:39 -0500

    moved Aja files to project packages

---
+++ src/main/java/com/finalproject/ArtHeistApp/repository/ShopperRepository.java
@@ -0,0 +1,9 @@
+package com.finalproject.ArtHeistApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Aja.Shopper;
+
+public interface ShopperRepository extends JpaRepository <Shopper, Integer>{
+
+}
