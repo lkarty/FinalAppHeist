7c4a3c9eb74bb69cd0f29b127052672afb441f41
Lindsay Karty <lmkarty@gmail.com>
2018-12-11 10:42:58 -0500

    removed some files we're no longer using

--- src/main/java/com/finalproject/ArtHeistApp/repository/DummyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.finalproject.ArtHeistApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.finalproject.ArtHeistApp.entities.Dummy;
-
-public interface DummyRepo extends JpaRepository <Dummy, Integer> {
-
-}
